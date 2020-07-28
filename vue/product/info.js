af60bf09e67f83f720d85275dabd072b8bce8e63
EspraDa <klicklove@gmail.com>
2020-07-29 02:46:11 +0900

    현재 선택한 카테고리 주소 생성

--- vue/product/info.js
+++ vue/product/info.js
@@ -30,13 +30,17 @@ var ProdInfoCate = {
         '      </select>\n' +
         '    </div>\n' +
         '    <div class="col-sm-2">\n' +
-        '      <select name="cno4" class="custom-select" size=10>\n' +
+        '      <select name="cno4" class="custom-select" size=10 v-model="detailCateSel">\n' +
         '        <option v-for="(option, index) in detailCategory"\n' +
         '                :key="index" :value="index">\n' +
         '          {{ option.text }}\n' +
         '        </option>\n' +
         '      </select>\n' +
         '    </div>\n' +
+        '  </div>\n' +
+        '  <div class="input-group">\n' +
+        '    <div class="col-sm-2">&nbsp;</div>\n' +
+        '    <div :class="currCateCheck">선택 카테고리 : <span>{{ currCategory }}</span></div>\n' +
         '  </div>\n' +
         '</div>',
     data: function() {
@@ -47,35 +51,69 @@ var ProdInfoCate = {
             middleCategory: [],
             smallCateSel: 0,
             smallCategory: [],
+            detailCateSel: 0,
             detailCategory: [],
+
+            allLargeCate: [],
+            allMiddleCate: [],
+            allSmallCate: [],
+            allDetailCate: [],
+
+            currCategory: '',
+            currCateCheck: 'col-sm-10',
         };
     },
     created: function() {
-        this.largeCategory = JSON.parse(sessionStorage.getItem('Prod_Cate_Large'));
+        this.allLargeCate   = JSON.parse(sessionStorage.getItem('Prod_Cate_Large'));
+        this.allMiddleCate  = JSON.parse(sessionStorage.getItem('Prod_Cate_Middle'));
+        this.allSmallCate   = JSON.parse(sessionStorage.getItem('Prod_Cate_Small'));
+        this.allDetailCate  = JSON.parse(sessionStorage.getItem('Prod_Cate_Detail'));
+
+        this.largeCategory  = this.allLargeCate;
     },
     watch: {
         largeCateSel: function() {
-            let allMiddleCate = JSON.parse(sessionStorage.getItem('Prod_Cate_Middle'));
-            this.middleCategory = allMiddleCate[this.largeCateSel];
+            let leaf = this.allLargeCate[this.largeCateSel]['leaf'];
+            this.middleCategory = this.allMiddleCate[this.largeCateSel];
             this.smallCategory = [];
             this.detailCategory = [];
+
+            this.currCateCheck = (leaf == 'T') ? 'col-sm-10' : 'col-sm-10 font-weight-bold';
+            this.currCategory = this.allLargeCate[this.largeCateSel]['text'];
         },
         middleCateSel: function() {
-            let allSmallCate = JSON.parse(sessionStorage.getItem('Prod_Cate_Small'));
-            this.smallCategory = allSmallCate[this.largeCateSel][this.middleCateSel];
+            let leaf = this.allMiddleCate[this.largeCateSel][this.middleCateSel]['leaf'];
+            this.smallCategory = this.allSmallCate[this.largeCateSel][this.middleCateSel];
             this.detailCategory = [];
+
+            this.currCateCheck = (leaf == 'T') ? 'col-sm-10' : 'col-sm-10 font-weight-bold';
+            this.currCategory =
+                this.allLargeCate[this.largeCateSel]['text'] + ' > ' +
+                this.allMiddleCate[this.largeCateSel][this.middleCateSel]['text'];
         },
         smallCateSel: function() {
-            let allSmallCate = JSON.parse(sessionStorage.getItem('Prod_Cate_Small'));
-            let leaf = allSmallCate[this.largeCateSel][this.middleCateSel][this.smallCateSel]['leaf'];
+            let leaf = this.allSmallCate[this.largeCateSel][this.middleCateSel][this.smallCateSel]['leaf'];
+
+            this.currCateCheck = (leaf == 'T') ? 'col-sm-10' : 'col-sm-10 font-weight-bold';
+            this.currCategory =
+                this.allLargeCate[this.largeCateSel]['text'] + ' > ' +
+                this.allMiddleCate[this.largeCateSel][this.middleCateSel]['text'] + ' > ' +
+                this.allSmallCate[this.largeCateSel][this.middleCateSel][this.smallCateSel]['text']
 
             if (leaf == 'T') {
-                let allDetailCate = JSON.parse(sessionStorage.getItem('Prod_Cate_Detail'));
-                this.detailCategory = allDetailCate[this.largeCateSel][this.middleCateSel][this.smallCateSel];
+                this.detailCategory = this.allDetailCate[this.largeCateSel][this.middleCateSel][this.smallCateSel];
             } else {
                 this.detailCategory = [];
             }
-        }
+        },
+        detailCateSel: function() {
+            this.currCateCheck = 'col-sm-10 font-weight-bold';
+            this.currCategory =
+                this.allLargeCate[this.largeCateSel]['text'] + ' > ' +
+                this.allMiddleCate[this.largeCateSel][this.middleCateSel]['text'] + ' > ' +
+                this.allSmallCate[this.largeCateSel][this.middleCateSel][this.smallCateSel]['text'] + ' > ' +
+                this.allDetailCate[this.largeCateSel][this.middleCateSel][this.smallCateSel][this.detailCateSel]['text'];
+        },
     },
 }
 
